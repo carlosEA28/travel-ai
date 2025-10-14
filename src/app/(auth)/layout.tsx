c74785cae62c2f11b86139dfd7b027cf690fc5fc
carlosEA28 <carloseaduarte28@gmail.com>
2025-10-13 22:23:48 -0300

    commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,5 @@
 // src/app/(onboarding)/layout.tsx
 import { ReactNode } from "react";
-import Template from "../template";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return <>{children}</>;
