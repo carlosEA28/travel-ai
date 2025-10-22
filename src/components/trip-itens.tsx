e00620a6e18db1ef0f537729eb75f8e35db3d769
carlosEA28 <carloseaduarte28@gmail.com>
2025-10-22 17:58:25 -0300

    remove save button

--- src/components/trip-itens.tsx
+++ src/components/trip-itens.tsx
@@ -78,9 +78,6 @@ const TripItensComponent = async ({ tripId }: { tripId: string }) => {
 
       <div className="mt-6 flex justify-between ">
         <div className="flex gap-2">
-          <Button className="bg-[#12A3ED] hover:bg-[#0E8DD0] transition-colors">
-            Save <Heart />
-          </Button>
           <Button variant={"secondary"} asChild>
             <Link href={`/trip/locations/${trip.id}`}>
               View in the map <MapPin />
