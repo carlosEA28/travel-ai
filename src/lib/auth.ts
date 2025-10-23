806a24acdab4b835afc30af95da22b53ecc70c02
carlosEA28 <carloseaduarte28@gmail.com>
2025-10-23 08:56:16 -0300

    fix better auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -19,4 +19,6 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
 });
